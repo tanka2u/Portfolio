50361adde3df910d9e735e337454161ece7ec93b
tanka2u <ptanka123@gmail.com>
2025-06-28 18:55:04 +0200

    Resume

---
+++ src/constants.js
@@ -0,0 +1,302 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import EagleEye from './assets/company_logo/EagleEye.png';
+import TukiSoft from './assets/company_logo/tukisoft logo.png';
+import Bishwa from './assets/company_logo/Bishwa.png';
+
+// Education Section Logo's
+import epita from './assets/education_logo/EPITA.png';
+import oxford from './assets/education_logo/oxford.png';
+import sapt from './assets/education_logo/Saptagandaki.jpg';
+import vpsLogo from './assets/education_logo/Bishwa.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import mernTaskmanager from './assets/work_logo/MERN-Taskmanager.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import food from './assets/work_logo/food.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: EagleEye,
+      role: "Fullstack Developer",
+      company: "Eagle Eye",
+      date: "May 2021 - Jan 2023",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: TukiSoft,
+      role: "Web Developer",
+      company: "Tuki Soft",
+      date: "Jan 2020 - Dec 2021",
+      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: Bishwa,
+      role: "Frontend Intern",
+      company: "Newton School",
+      date: "Jan 2020 - Dec 2021",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: epita,
+      school: "EPITA, l'école des ingénieurs en intelligence informatique, Le Kremlin-Bicêtre, France",
+      date: "Feb 2023 - Feb 2024",
+      grade: "Ongoing",
+      desc: "I am currently my Master's degree in MSc. is Software Engineering from EPITA, France. During my time at EPITA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at EPITA has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "MSc. is Software Engineering",
+    },
+    {
+      id: 1,
+      img: oxford,
+      school: "Oxford College of Engineering and Management, Nawalparasi, Nepal",
+      date: "Sept 2009 - Sept 2016",
+      grade: "2.45 CGPA",
+      desc: "I completed my Bachelor's degree in Oxford College of Engineering and Management, Nawalparasi, Nepal. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at OXFORD College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor in Computer Engineering - BCA (Computer Science)",
+    },
+    {
+      id: 2,
+      img: sapt,
+      school: "Saptagandaki Multiple Campus, Chitwan, Nepal",
+      date: "Apr 2006 - March 2008",
+      grade: "Second Division",
+      desc: "I completed my class 12 education from Saptagandaki Multiple Campus, Chitwan, Nepal, under the HSEB board, where I studied Management, and Mathematics (PCM) with Computer Science.",
+      degree: "HSEB(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: vpsLogo,
+      school: "Bishwa Prakash Higher Secondary School, Chitwan, Nepal",
+      date: "April 1998 - March 2005",
+      grade: "First Division",
+      desc: "I completed my class 10 education from Bishwa Prakash Higher Secondary School, Chitwan, Nepal, under the SLC board, where I studied Science with Opt. Mathematics.",
+      degree: "SLC(X), Science with Opt. Mathematics",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "GitHub Profile Detective",
+      description:
+        "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+      github: "https://github.com/tanka2u/GitHub-Profile-Search-App-Using-React-JS",
+      webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "CS Prep",
+      description:
+        "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+      image: csprepLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/tanka2u/CSPrep",
+      webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/tanka2u/Movie-Recommendation-App",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "MERN Task-manager",
+      description:
+        "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+      image: mernTaskmanager,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/tanka2u/MERN-Taskmanager",
+      webapp: "https://www.npmjs.com/package/cmtk-email-validatorhttps://new-taskmanager.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/tanka2u/Task-Reminder-Tool",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "Webverse Digital",
+      description:
+        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/tanka2u/Webverse-Digital",
+      webapp: "https://webversedigital.com/",
+    },
+        {
+      id: 6,
+      title: "Order a food",
+      description:
+        "The official website for ordering food, a creative food ordering app. Built using HTML, CSS, JavaScript and react it features visually appealing animations and a clean design to showcase the food's services.",
+      image: food,
+      tags: ["HTML", "CSS", "JavaScript", "react", "animation"],
+      github: "https://github.com/tanka2u/Food-Order",
+      webapp: "https://foodsincart.netlify.app/",
+    },
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/tanka2u/Image-Search-App",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "https://github.com/tanka2u/Image-Background-Remover",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];  
